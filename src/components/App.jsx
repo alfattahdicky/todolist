77a644538a77d80b6909121dbe6b1b0042db620d
alfattahdicky <alfattahdicky2@gmail.com>
2022-06-20 01:27:43 +0700

    using Components & props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import '../index.css'
+import Header from './Header';
 
 class App extends React.Component {
   constructor(props) {
-  	super(props);
+		super(props);
 
-  	this.state = {count: 0}
+		this.state = {count: 0}
   }
 
   handleCount(){
-  	this.setState({count: this.state.count += 1});
+		this.setState({count: this.state.count += 1});
   }
 
   render() {
     return (
-    	<div>
-    		<h1>Hello World</h1>
-      		<button onClick={() => {this.handleCount()}}>
-      			{this.state.count}
-      		</button>
-    	</div>
+			<div>
+				<Header 
+					name = "Dicky"
+				/>
+				<p>{this.state.count}</p>
+				<button onClick={() => {this.handleCount()}}>tambah</button>
+			</div>
     );    
   };
 }
